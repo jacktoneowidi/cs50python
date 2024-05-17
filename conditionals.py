c103072c8229ab3065541a710ae57c299e6cde68
owidi <owidijacktone@gmail.com>
2024-05-17 08:15:49 -0700

    learning functions and string

--- conditionals.py
+++ conditionals.py
@@ -1,2 +1,28 @@
+# asking user their name
 name = input('whats your name ')
 print ( 'hii ' + name)
+
+
+
+
+print("hello 'friend'")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,4 @@
+print('hello world')
+print('i am iron man')
+print('i am tonny stark')
+print('i am poppy')
